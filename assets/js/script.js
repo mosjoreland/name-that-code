c6c4d741920042b1e072899eb3f73f083684624a
mosjoreland <jkw.42699@gmail.com>
2023-03-20 15:30:58 -0700

    added the HTML code

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        Timer: <span id="timeLeft">0</span>
+    </header>
+
+    <h2 id="question">Welcome to my code quiz</h2>
+
+    <div id="instruction">
+        <p>
+            Please click start below to begin quiz.
+        </p>
+    </div>
+
+    <button id="start">Start quiz</button>
+    
+    <script src="./script.js"></script>
+</body>
+</html>

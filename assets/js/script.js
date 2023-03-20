5f78ee5a3baceadb261ebc743e8711da3b2d79d1
mosjoreland <jkw.42699@gmail.com>
2023-03-20 15:32:01 -0700

    started the javascript code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,68 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <header>
-        Timer: <span id="timeLeft">0</span>
-    </header>
+let timeLeftEl = document.querySelector('#timeLeft');
+let headingEl = document.querySelector('#question');
+let contentEl = document.querySelector('#instruction');
+let startBtnEl = document.querySelector('#start');
+let timer;
 
-    <h2 id="question">Welcome to my code quiz</h2>
+// list of all questions, choices, and answers
 
-    <div id="instruction">
-        <p>
-            Please click start below to begin quiz.
-        </p>
-    </div>
+let questions = [
+  {
+    title: 'Commonly used data types DO NOT include:',
+    choices: ['strings', 'booleans', 'alerts', 'numbers'],
+    answer: 'alerts',
+  },
+  {
+    title: 'The condition in an if / else statement is enclosed within ____.',
+    choices: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
+    answer: 'parentheses',
+  },
+  {
+    title: 'Arrays in JavaScript can be used to store ____.',
+    choices: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above',
+    ],
+    answer: 'all of the above',
+  },
+  {
+    title:
+      'String values must be enclosed within ____ when being assigned to variables.',
+    choices: ['commas', 'curly brackets', 'quotes', 'parentheses'],
+    answer: 'quotes',
+  },
+  {
+    title:
+      'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choices: ['JavaScript', 'terminal / bash', 'for loops', 'console.log'],
+    answer: 'console.log',
+  },
+];
 
-    <button id="start">Start quiz</button>
+let timeLeft = 60;
+let indexOfCurrentQuestion = 0;
+
+function renderNextQuestion() {
+    contentEl.innerHTML = '';
+    let currentQuestion = questions[indexOfCurrentQuestion];
+
+    headingEl.textContent = currentQuestion.title;
     
-    <script src="./script.js"></script>
-</body>
-</html>
+
+    for (let i = 0; i < currentQuestion.choices.length; i++) {
+        let buttonEl = document.querySelector('button');
+        buttonEl.setAttribute('class', 'choice')
+        buttonEl.textContent = currentQuestion.choices[i];
+        contentEl.appendChild(buttonEl);
+    }
+}
+
+startBtnEl.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  timer = setInterval(function () {
+    console.log('Timer started!');
+  }, 1000);
+})
